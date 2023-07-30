7f043554a24e947cb988c11315ec2f5e75f6095e
Iago-Santos-Sousa <iago.santos.sousa@gmail.com>
2023-07-29 23:16:17 -0300

    versão 1.0

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import Loader from "./components/Loader";
+
+interface DataIp {
+  ip: string;
+}
+
+function App(): JSX.Element {
+  const [data, setData] = useState<DataIp | null>(null);
+  const [loader, setLoader] = useState<boolean>(false);
+
+  const getAdressIp = async () => {
+    setLoader((load) => !load);
+    const response = await fetch("https://ip4.seeip.org/json");
+    const dataResponse = await response.json();
+    console.log(dataResponse);
+    const { ip } = dataResponse;
+    setData({ ip });
+    setLoader((load) => !load);
+  };
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => getAdressIp()}>Exibir ip</button>
+      </div>
+      {loader ? (
+        <Loader />
+      ) : data ? (
+        <p>{data.ip}</p>
+      ) : !data ? null : (
+        <p>Erro!</p>
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Loader.tsx
@@ -0,0 +1,11 @@
+import { RiLoader2Line } from "react-icons/ri";
+
+const Loader = (): JSX.Element => {
+  return (
+    <div className="loader">
+      <RiLoader2Line />
+    </div>
+  );
+};
+
+export default Loader;
